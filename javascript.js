31aec7e174aba08a20115b113340d256c12c8829
Lusks <lucasnovaesprog@hotmail.com>
2021-04-29 01:06:33 -0300

    Condicionais programadas com as devidas exceções

--- javascript.js
+++ javascript.js
@@ -2,15 +2,30 @@ function calculoTroco() {
     var valorPago = window.document.getElementById('valorPago')
     var custo = window.document.getElementById('custo')
     var troco = window.document.getElementById('troco')
-    var vp = Number(valorPago.value)
-    var c = Number(custo.value)
+    var vp = Number(valorPago.value) //conversão do valorPago de texto para número
+    var c = Number(custo.value) //conversão do custo de texto para número
     var resultado = vp - c
-    troco.innerHTML = `O seu troco é de <strong>R$${resultado}</strong>!`
-    cardJs.style.backgroundColor = 'green';
+    if (vp > c) {
+        troco.innerHTML = `O seu troco é de <strong>R$${resultado}</strong>!`
+        cardJs.style.backgroundColor = 'green';
+    } else {
+        if (vp < c) {
+            window.alert('Números incorretos! Tente novamente.')
+            return;
+        }
+    }
+}
 
-    if (vp < c) {
-        window.alert('Números incorretos!');
-        return;
+function somenteNumeros(dadoInserido) {
+    var numeros = /^[0-9]+$/;
+    if (dadoInserido.value.match(numeros)) {
+        document.form1.text1.focus();
+        return true;
+    }
+    else {
+        window.alert('Somente números são válidos. Por favor, tente novamente.');
+        document.form1.text1.focus();
+        return false;
     }
 }
 
